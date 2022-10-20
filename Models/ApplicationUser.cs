2ddde1fb85a6a53b3af3ff38411c45050f02dddf
Rodrigo82Araya <araya.r82@gmail.com>
2022-10-19 23:19:09 -0300

    Carga inicial

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TransportesMR.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        //[Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+
+
+    }
+}
